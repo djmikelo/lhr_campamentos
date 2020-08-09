4ad5e85f6e1dffc2806d83d9a4c695e88e373bc8
djmikelo <66913771+djmikelo@users.noreply.github.com>
2020-08-09 22:52:38 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,4 +5,3 @@ games { 'rdr3', 'gta5' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 client_script {'client.lua','config.lua','warmenu.lua','menu.lua'}
-server_script {'server.lua'}
